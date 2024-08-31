883cfadf0271512aa7d1c5ea701bac79cb2d50e3
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2024-08-31 20:55:59 -0300

    Haskell

--- main.hs
+++ main.hs
@@ -1,3 +1,11 @@
+-- Accumulators
+myFac n = aux n 1
+    where
+        aux n acc
+            | n <= 1 = acc
+            | otherwise = aux (n - 1) (n * acc)
+
+-- Function definition
 addTuples :: [(Int, Int)] -> [Int]
 addTuples xs = [ x + y | (x, y) <- xs ]
 
