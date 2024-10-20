3ff1b798aeb2e02e929e7a52ff4c2928585d1ce1
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2024-10-20 08:43:52 -0300

    Haskell (challenge 1)

--- haskell-course/haskell-benchmarking-api/app/Main.hs
+++ haskell-course/haskell-benchmarking-api/app/Main.hs
@@ -4,6 +4,24 @@
 import Web.Scotty
 
 import Data.Monoid (mconcat)
+import Data.Aeson (ToJSON, toJSON, object, (.=))
+
+newtype LargestPrimeFactorOutput = LargestPrimeFactorOutput { value :: Int } deriving (Show)
+
+instance ToJSON LargestPrimeFactorOutput where
+    toJSON (LargestPrimeFactorOutput value) =
+        object ["value" .= value]
+
+primeFactors :: Int -> [Int]
+primeFactors n = factor n 2
+    where
+        factor 1 _ = []
+        factor m k
+            | m `mod` k == 0 = k : factor (m `div` k) k
+            | otherwise      = factor m (k + 1)
+
+largestPrimeFactor :: Int -> Int
+largestPrimeFactor n = maximum (primeFactors n)
 
 -- Main entry point 
 main :: IO()
@@ -14,9 +32,6 @@ main = scotty 3000 $ do
     get "/" $ do
         html "<h1>haskell-benchmarking-api</h1>"
 
-    get "/largest-prime-factor" $ do
-        -- TODO: Implement a solution to this problem: https://projecteuler.net/problem=3
-        -- TODO: Implement a solution to this problem: https://projecteuler.net/problem=3
-        -- TODO: Implement a solution to this problem: https://projecteuler.net/problem=3
-
-        html "<h1>haskell-benchmarking-api</h1>"
+    get "/largest-prime-factor/:number" $ do
+        number <- param "number" :: ActionM Int
+        json $ LargestPrimeFactorOutput (largestPrimeFactor number)
